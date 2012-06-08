e586e8163ef98881aae1b629f62e8a118b75b6a1
Nicolas Pierron <nicolas.b.pierron@mozilla.com>
2012-06-08 14:29:45 -0700

    Ensure fp is not an eval frame before checking the number of actual arguments

--- js/src/ion/Ion.cpp
+++ js/src/ion/Ion.cpp
@@ -876,7 +876,7 @@ Compile(JSContext *cx, JSScript *script, js::StackFrame *fp, jsbytecode *osrPc)
     JS_ASSERT_IF(osrPc != NULL, (JSOp)*osrPc == JSOP_LOOPENTRY);
 
     // Skip if there is too much arguments.
-    if (fp->isFunctionFrame() && fp->numActualArgs() <= js_IonOptions.maxStackArg)
+    if (fp->hasArgs() && fp->numActualArgs() <= js_IonOptions.maxStackArg)
         return Method_Skipped;
 
     if (cx->compartment->debugMode()) {
